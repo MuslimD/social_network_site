0b725dc970b7e6fa799b29acffe07bf08083fbff
muslim <mhas2506@gmail.com>
2023-01-18 10:14:24 +0300

    Navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import Interesting from "./Interesting/Interesting";
 import SignIn from "./Authorization/SignIn";
 import Header from "./Header/Header";
 import Navbar from "./Navbar/Navbar";
-import styles from "./App.module.scss";
-import { useSelector } from "react-redux";
 import SignUp from "./Authorization/SignUp";
-import { Route, Routes } from "react-router-dom";
 import Mypage from "./Mypage/Mypage";
-import Interesting from "./Interesting/Interesting";
 import Message from "./Message/Message";
 import Followers from "./Followers/Followers";
 import Follows from "./Follows/Follows";
+import styles from "./App.module.scss";
+
 function App() {
   const token = useSelector((state) => state.userSlice.token);
   if (token) {
