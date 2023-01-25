f5f6fa92ca09eae21d10c9d6220180e843108aeb
grozny <h6umid@gmail.com>
2023-01-25 10:47:29 +0300

    подписки подписчики

--- src/features/followersSlice.js
+++ src/features/followersSlice.js
@@ -1,38 +1,50 @@
-import {  createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  followers: []
+    followers: [],
+    followersErr: null,
+    followersLoad: false,
 };
 
-// export const getmessages = createAsyncThunk(
-//   "get/message",
-//   async ( {chatsId}, thunkApi) => {
-//     try {
-//       const res = await fetch(`http://localhost:4000/messages/${chatsId}`);
-//       const messages = await res.json(res);
-//       if (messages.error) {
-//         return thunkApi.rejectWithValue(messages);
-//       }
-//       return thunkApi.fulfillWithValue(messages);
-//     } catch (error) {
-//       return thunkApi.rejectWithValue(error.message);
-//     }
-//   }
-// );
+export const followersget = createAsyncThunk(
+    "get/followers",
+    async ({ userid }, thunkApi) => {
+        try {
+            const res = await fetch(`http://localhost:4000/followers/${userid}`);
+            const followers = await res.json(res);
+            if (followers.error) {
+                return thunkApi.rejectWithValue(followers);
+            }
+            return thunkApi.fulfillWithValue(followers);
+        } catch (error) {
+            return thunkApi.rejectWithValue(error.message);
+        }
+    }
+);
 
 
 
 
 const followersSlice = createSlice({
-  name: "followers",
-  initialState,
-  reducers: {},
-  extraReducers: (buider) => {
-    buider
+    name: "followers",
+    initialState,
+    reducers: {},
+    extraReducers: (buider) => {
+        buider
 
-      .addCase()
-  
-  },
+            .addCase(followersget.rejected, (state, action) => {
+                state.followersErr = action.payload
+                state.followersLoad = false
+            }).addCase(followersget.pending, (state) => {
+                state.followersErr = null
+                state.followersLoad = true
+            }).addCase(followersget.fulfilled, (state, action) => {
+                state.followers = action.payload.followers
+                state.followersErr = null
+                state.followersLoad = false
+            })
+
+    },
 });
 
 export default followersSlice.reducer;
