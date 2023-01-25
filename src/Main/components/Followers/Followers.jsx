7ffcf381ab320434ed8a1ada0512a6b55786eaa8
arthurshelbaev <konodiobrandoda@mail.ru>
2023-01-25 12:24:47 +0300

    Delete src/Main/components directory

--- src/Main/components/Followers/Followers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Followers = (props) => {
-    return (
-      <>jgvfguvfdfy</>
-    );
-};
-
-export default Followers;
--- src/Main/components/Follows/Follows.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Follows = (props) => {
-    return (
-        <>hbfvgvug</>
-    );
-};
-
-export default Follows;
