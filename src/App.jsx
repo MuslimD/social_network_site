aca3d769d27e56b139115fa1fbd64fa7da63fd6e
Muslim <mdovletmerzayev@bk.ru>
2023-12-28 17:17:24 +0300

    11

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
             <Route path="/messagepage" element={<Messager  userid={id}/>} />
             <Route path="/followerspage" element={<Followers userid={id} />} />
             <Route path="/followspage" element={<Follows userid={id} />} />
+            <Route path="/followspage" element={<Follows userid={id} />} />
           </Routes>
         </div>
       </>
